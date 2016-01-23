0c6a33dd180cafb6c03501d4cf337a705ed11f4a
stefanovtamara <stefanov.tamara@gmail.com>
2016-01-23 13:10:26 -0500

    Add show contact

--- crm.rb
+++ crm.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require_relative 'contact.rb'
 
+#TEMP id
+Contact.create('Johnny','Bravo', 'johnny@bitmakerlabs.com', 'Rockstar')
+
 get '/' do
   @crm_app_name = "Tamara's CRM"
   erb :index
@@ -17,4 +20,9 @@ end
 post '/contacts' do
   Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
   redirect to('/contacts')
+end
+
+get '/contacts/1' do
+  @contact = Contact.get(1)
+  erb :show_contact
 end
